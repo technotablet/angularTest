2c82f8d949f86f0ca560e21142d8d457a43c1c6e
Vivek Kapoor <vivek@exain.com>
2018-07-22 15:57:20 +0530

    Angular Universal - a Server which serves pages which look like static html. Next step is to implement prerendering

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HeaderComponent } from './header/header.component';
-import { UsersComponent } from './users/users.component';
-import { PostsComponent } from './posts/posts.component';
-import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { SidebarComponent } from "./sidebar/sidebar.component";
+import { HeaderComponent } from "./header/header.component";
+import { UsersComponent } from "./users/users.component";
+import { PostsComponent } from "./posts/posts.component";
+import { HomeComponent } from "./home/home.component";
+
+import { PLATFORM_ID, APP_ID, Inject } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -19,10 +22,20 @@ import { HomeComponent } from './home/home.component';
     HomeComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: "static-site" }),
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(APP_ID) private appId: string
+  ) {
+    const platform = isPlatformBrowser(platformId)
+      ? "in the browser"
+      : "on the server";
+    console.log(`Running ${platform} with appId=${appId}`);
+  }
+}
